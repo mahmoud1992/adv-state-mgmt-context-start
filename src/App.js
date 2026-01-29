bcd11a1e0b18044ac991a0df24ead7d07edad0c1
Mahmoud Alsaqqa <alsfeer.m1992m@gmail.com>
2026-01-29 17:37:31 +0000

    Creating & Providing The Context

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 import Header from "./components/Header";
 import Shop from "./components/Shop";
 import { DUMMY_PRODUCTS } from "./dummy-products.js";
+import Product from "./components/Product";
 
 function App() {
   const [shoppingCart, setShoppingCart] = useState({
@@ -71,7 +72,13 @@ function App() {
         cart={shoppingCart}
         onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
       />
-      <Shop onAddItemToCart={handleAddItemToCart} />
+      <Shop>
+        {DUMMY_PRODUCTS.map((product) => (
+          <li key={product.id}>
+            <Product {...product} onAddToCart={handleAddItemToCart} />
+          </li>
+        ))}
+      </Shop>
     </>
   );
 }
